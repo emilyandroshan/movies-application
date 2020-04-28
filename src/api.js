5e98476fc9880383eddf6c819db8920489096652
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-04-28 14:53:08 -0500

    creating postMovies function template to import into index.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,23 @@
 module.exports = {
   getMovies: () => {
-    return fetch('http://localhost:1313/api/movies')
+    return fetch('api/movies')
       .then(response => response.json());
   }
 };
-  // postMovies: () => {
-  //
-  // }
+  postMovies: () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        },
+      body: JSON.stringify(db.json)
+      };
+    fetch('api/movies', options)
+        .then(response => response.json())
+        .then()
+  };
+
+
 //
 // const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
 // const url = '/posts';
--- src/index.js
+++ src/index.js
@@ -8,21 +8,16 @@ sayHello('World');
  * require style imports
  */
 // const {getMovies} = require('./api.js');
-import {getMovies} from './api.js';
+import {getMovies, postMovies} from './api.js';
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
-  //TRYING TO SOLVE THE SECOND .then problem
-  // movies.forEach(({title, rating, id}) => {
-  //   console.log(`id#${id} - ${title} - rating: ${rating}`);
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
 
-//DECIDE TO USE VANILLA JS OR JQUERY FOR OUTPUT TO RENDER DATA
-//JQUERY NEEDS SCRIPT????
-// document.getElementsByClassName('container').innerHTML =
+
