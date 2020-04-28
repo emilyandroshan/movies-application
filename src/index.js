2debb223ba14e659be829109107fdf1834f483db
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-04-28 12:32:41 -0500

    working on second .then and how to render data to browser

--- src/index.js
+++ src/index.js
@@ -14,7 +14,14 @@ getMovies().then((movies) => {
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
+  //TRYING TO SOLVE THE SECOND .then problem
+  // movies.forEach(({title, rating, id}) => {
+  //   console.log(`id#${id} - ${title} - rating: ${rating}`);
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+//DECIDE TO USE VANILLA JS OR JQUERY FOR OUTPUT TO RENDER DATA
+//JQUERY NEEDS SCRIPT????
+// document.getElementsByClassName('container').innerHTML =
